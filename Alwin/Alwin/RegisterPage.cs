9158e02abfc147d4842334d1504ec448a4ad7fff
TomPallister <tom.pallister@fourth.com>
2014-10-28 09:36:41 +0000

    first commit

---
+++ Alwin/Alwin/RegisterPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Alwin
+{
+    public class RegisterPage
+    {
+        private IWebDriver _driver;
+
+        public RegisterPage(IWebDriver driver, string url)
+        {
+            _driver = driver;
+            _driver.Navigate().GoToUrl(url);
+
+        }
+
+        public void EnterUserName(string userName)
+        {
+            _driver.FindElement(By.Id("MainContent_RegisterUser_CreateUserStepContainer_UserName")).SendKeys(userName);
+        }
+    }
+}
---
+++ Alwin/Alwin/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Alwin
+{
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://fourthqaportal.cloudapp.net/technicaltest/Account/Register");
+            driver.FindElement(By.Id("MainContent_RegisterUser_CreateUserStepContainer_UserName")).SendKeys("user_name");
+            driver.FindElement(By.Id("MainContent_RegisterUser_CreateUserStepContainer_Email")).SendKeys("user@fourthdev.com");
+            driver.FindElement(By.Id("MainContent_RegisterUser_CreateUserStepContainer_Password")).SendKeys("password");
+            driver.FindElement(By.Id("MainContent_RegisterUser_CreateUserStepContainer_ConfirmPassword")).SendKeys("password");
+            driver.FindElement(By.Name("ctl00$MainContent$RegisterUser$CreateUserStepContainer$ctl09")).Click();
+        }
+
+        [Test]
+        public void TestTwo()
+        {
+            IWebDriver driver = new ChromeDriver();
+            RegisterPage registerPage = new RegisterPage(driver, "http://fourthqaportal.cloudapp.net/technicaltest/Account/Register");
+            registerPage.EnterUserName("AUser");
+        }
+    }
+}
